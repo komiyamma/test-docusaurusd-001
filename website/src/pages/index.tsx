6b746568859908092ca53c1be55e2d5094c15290
komiyamma <komiyama0629@gmail.com>
2026-01-11 20:11:18 +0900

    refactor(website): ホームページヘッダーを削除

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,33 +1,7 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/react-study/react_index">
-            React学習を始める ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 import { WebSiteJsonLd } from '../components/SEO/StructuredData';
 
@@ -38,7 +12,6 @@ export default function Home(): ReactNode {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <WebSiteJsonLd />
-      <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
